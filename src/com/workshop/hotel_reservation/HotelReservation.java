07b0b24a838d7b4afe3348d3c094a60bad6002c0
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-18 06:28:35 +0530

    Add:Program to add weekday and weekend rates to hotels added

--- src/com/workshop/hotel_reservation/HotelReservation.java
+++ src/com/workshop/hotel_reservation/HotelReservation.java
@@ -10,71 +10,72 @@ public class HotelReservation {
 
 	 public static void main(String[] args) throws IOException {
 		System.out.println("Welcome To Hotel Reservation Program");
-		System.out.println("Enter date  ex. 16Mar2020(mon)");
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String date = bf.readLine();
+		
+		//date pattern to be entered
+		 System.out.println("Enter date  ex. 16Mar2020(mon)");
+         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+         String date = bf.readLine();
 
-		
-		//setting values for lakewood
-	    lake = new Hotel("Lakewood");
-	    lake.setRegularWeekDay(110);
-	    
-		 		
-		//setting values for bridgewood
-	    bridge = new Hotel("Bridgewood");
-	    bridge.setRegularWeekDay(160);
-	   
-				
-		//setting values for ridgewood   
-	    ridge = new Hotel("Ridgewood");
-	    ridge.setRegularWeekDay(220);
-	    
-		
-		 //int index = date.indexOf(":");
-	     //String type = date.substring(0, index);
-	     int cost_lake = 0, cost_bridge = 0, cost_ridge = 0;
-	     int day_index_start = 0, day_index_end = 0;
-	     
-	    while (day_index_start != -1) {
-		     day_index_start = date.indexOf("(", day_index_start + 1);
-		     day_index_end = date.indexOf(")", day_index_end + 1);
-		
-		     if (day_index_start != -1) {
-			     String day = date.substring(day_index_start + 1, day_index_end);
-				    
-					     cost_lake += lake.getRegularWeekDay();
-					     cost_bridge += bridge.getRegularWeekDay();
-					     cost_ridge += ridge.getRegularWeekDay();
-				     
-			     }
-		     }  
-	    
-	     System.out.println("\nLakewood : "+cost_lake+"$");
+ 		
+ 		//setting values for lakewood
+ 	    lake = new Hotel("Lakewood");
+ 	    lake.setRegularWeekDay(110);
+ 	    lake.setRegularWeekEnd(90);
+ 		 		
+ 		//setting values for bridgewood
+ 	    bridge = new Hotel("Bridgewood");
+ 	    bridge.setRegularWeekDay(150);
+ 	    bridge.setRegularWeekEnd(50);
+ 				
+ 		//setting values for ridgewood   
+ 	    ridge = new Hotel("Ridgewood");
+ 	    ridge.setRegularWeekDay(220);
+ 	    ridge.setRegularWeekEnd(150);
+ 		
+ 		 //int index = date.indexOf(":");
+ 	     //String type = date.substring(0, index);
+ 	     int cost_lake = 0, cost_bridge = 0, cost_ridge = 0;
+ 	     int day_index_start = 0, day_index_end = 0;
+ 	     
+ 	    while (day_index_start != -1) {
+ 		     day_index_start = date.indexOf("(", day_index_start + 1);
+ 		     day_index_end = date.indexOf(")", day_index_end + 1);
+ 		
+ 		     if (day_index_start != -1) {
+ 			     String day = date.substring(day_index_start + 1, day_index_end);
+ 				    
+ 					     cost_lake += lake.getRegularWeekDay();
+ 					     cost_bridge += bridge.getRegularWeekDay();
+ 					     cost_ridge += ridge.getRegularWeekDay();
+ 				     
+ 			     }
+ 		     }  
+ 	    //printing hotel costs
+ 	     System.out.println("\nLakewood : "+cost_lake+"$");
 	     System.out.println("Bridgewood : "+cost_bridge+"$");
 	     System.out.println("Ridgewood : "+cost_ridge+"$");
-	
+	     //printing hotel with chepeast price
 	     String result = min(cost_lake, cost_bridge, cost_ridge);
 	     System.out.println("Cheapest price is of "+result);
-    	}
-    	
-     //implementing the min method
-     private static String min(int a, int b, int c) {
-	     if (a < b && a < c) {
-	    	 return lake.getHotelName();
-	     } else if (b < a && b < c) {
-	    	 return bridge.getHotelName();
-	     } else if (c < a && c < b) {
-	    	 return ridge.getHotelName();
-	     } else if (a == b || b == c) {
-	    	 return bridge.getHotelName();
-	     } else if (c == a) {
-	    	 return ridge.getHotelName();
-	     } else {
-	    	 return lake.getHotelName();
-	     } 
-	}
+     	}
+     	
+   //implementing the min method to find hotel with cheapest price
+     public static  String min(int a, int b, int c) {
+ 		if(a < c && a < c) {
+ 			return lake.getHotelName();
+ 		}else if(b<a && b < c) {
+ 			return bridge.getHotelName();
+ 		}else if(a==b ) {
+ 			return (lake.getHotelName()+" "+bridge.getHotelName());
+ 		}else if(b==c) {
+ 			return (bridge.getHotelName()+" "+ridge.getHotelName());
+ 		}else if(a==c) {
+ 			return (lake.getHotelName()+" "+ridge.getHotelName());
+ 		}
+ 		else {
+ 			return ridge.getHotelName();
+ 		}
+ 	}
 }
-
-
 	
 
