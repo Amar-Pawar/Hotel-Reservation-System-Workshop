ce8bdddb99747196a5b2653622061034ec46d595
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-18 06:04:37 +0530

    Add:Welcome message added

---
+++ src/com/workshop/hotel_reservation/HotelReservation.java
@@ -0,0 +1,8 @@
+package com.workshop.hotel_reservation;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Hotel Reservation Program");
+	}
+}
