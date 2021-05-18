860580ec2cfb0946cceb3325dcb395402d9d96fc
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-18 06:13:17 +0530

    Add:Hotel names and rates for regular customers added

---
+++ src/com/workshop/hotel_reservation/Hotel.java
@@ -0,0 +1,31 @@
+package com.workshop.hotel_reservation;
+
+public class Hotel {
+	
+	String hotelName;
+    private int regularWeekDay;
+    private int regularWeekEnd;
+    
+	public Hotel(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public int getRegularWeekDay() {
+		return regularWeekDay;
+	}
+	public void setRegularWeekDay(int regularWeekDay) {
+		this.regularWeekDay = regularWeekDay;
+	}
+	public int getRegularWeekEnd() {
+		return regularWeekEnd;
+	}
+	public void setRegularWeekEnd(int regularWeekEnd) {
+		this.regularWeekEnd = regularWeekEnd;
+	}
+	
+}
--- src/com/workshop/hotel_reservation/HotelReservation.java
+++ src/com/workshop/hotel_reservation/HotelReservation.java
@@ -1,8 +1,39 @@
 package com.workshop.hotel_reservation;
+import java.io.IOException;
+
 
 public class HotelReservation {
+	//declaring variables for respective hotels
+	 private static Hotel lake, bridge, ridge;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Hotel Reservation Program");
+		 hotelDetails();
 	}
-}
+	
+	//method to set details for each hotel
+	  public static void hotelDetails() {
+	 		System.out.println("Hotels and their Prices for Regular Customer \n");
+	 		
+	 		//setting values for lakewood
+	 	    lake = new Hotel("Lakewood");
+	 	    lake.setRegularWeekDay(110);
+	 	    lake.setRegularWeekEnd(90);
+	 		System.out.println("HotelName: "+ lake.getHotelName()+"\n"+"Regular WeekDay Price: "+lake.getRegularWeekDay()+"$\n"+"Regular WeekEnd Price: "+lake.getRegularWeekEnd()+"$\n");
+	 		
+	 		//setting values for bridgewood
+	 	    bridge = new Hotel("Bridgewood");
+	 	    bridge.setRegularWeekDay(160);
+	 	    bridge.setRegularWeekEnd(60);;
+	 		System.out.println("HotelName: "+ bridge.getHotelName()+"\n"+"Regular WeekDay Price: "+bridge.getRegularWeekDay()+"$\n"+"Regular WeekEnd Price: "+bridge.getRegularWeekEnd()+"$\n");
+	 		
+	 		//setting values for ridgewood   
+	 	    ridge = new Hotel("Ridgewood");
+	 	    ridge.setRegularWeekDay(220);
+	 	    ridge.setRegularWeekEnd(150);
+	 		System.out.println("HotelName: "+ ridge.getHotelName()+"\n"+"Regular WeekDay Price: "+ridge.getRegularWeekDay()+"$\n"+"Regular WeekEnd Price: "+ridge.getRegularWeekEnd()+"$\n");
+	 			
+	 	}
+ }
+
+
